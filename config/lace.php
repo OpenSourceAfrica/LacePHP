7437e890f3fced9b774494ca9be574e5fbec29e2
Akinyele Olubodun <akinyeleolubodun@yahoo.com>
2025-06-30 02:26:15 +0100

    Complete rebasing

--- config/lace.php
+++ config/lace.php
@@ -14,8 +14,8 @@ return [
         'mysql' => [
             'host'     => env('DB_HOST', '127.0.0.1'),
             'port'     => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'lace'),
+            'username' => env('DB_USERNAME', 'lace'),
             'password' => env('DB_PASSWORD', ''),
             'charset'  => env('DB_CHARSET', 'utf8mb4'),
             'collation'=> env('DB_COLLATION', 'utf8mb4_unicode_ci'),
